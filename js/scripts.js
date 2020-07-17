181316783fa5e17e54ae093be58bbac0174ee196
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2020-07-17 14:15:25 -0400

    Add toLocaleDateString() method to date object

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,5 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
-var d = new Date(2020, 6, 17, 18, 30);
-document.getElementById("date").innerHTML = 'Last updated: ' + d;
+var date = new Date(2020, 6, 17).toLocaleDateString();
+document.getElementById("date").innerHTML = 'Last updated: ' + date;
