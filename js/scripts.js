4eaa569d0e6b428c085f9b6f8d40245661cca54f
wadams-dev <50535899+wadams-dev@users.noreply.github.com>
2020-07-17 13:38:03 -0400

    Add date functionality

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,6 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+var d = new Date(2020, 6, 17, 18, 30);
+document.getElementById("date").innerHTML = 'Last updated: ' + d;
